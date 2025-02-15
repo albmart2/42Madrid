830c1ce4005d0a52969d3a3c058b3426cb92893c
albmart2 <martinmunoz.alba@gmail.com>
2025-02-15 18:09:19 +0100

    update ft_printf.c and ft_printf_utils.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,14 +6,50 @@
 /*   By: albmart2 <albmart2@student.madrid42>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 17:59:39 by albmart2          #+#    #+#             */
-/*   Updated: 2025/01/29 18:01:06 by albmart2         ###   ########.fr       */
+/*   Updated: 2025/02/15 18:05:09 by albmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdarg.h>
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-    va_list args;
-    
+	va_list	args;
+	int	i;
+
+	va_start (args, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+				ft_putchar(va_arg(args, int), 1);
+			else if (format[i] == 's')
+				ft_putstr_fd(va_arg(args, char *), 1);
+			else if (format[i] == 'd')
+				ft_putnbr_fd(va_arg(args, int), 1);
+			else if (format[i] == 'x')
+				ft_putnbr_base_fd(va_arg(args, unsigned int),
+					"0123456789abcdef", 1);
+			else if (format[i] == 'X')
+				ft_putnbr_base_fd(va_arg(args, unsigned int),
+					"0123456789ABCDEF", 1);
+			else if (format[i] == 'p')
+			{
+				ft_putstr_fd("0x", 1);
+				ft_putnbr_base_fd(va_arg(args, unsigned long),
+					"0123456789abcdef", 1);
+			}
+			i++;
+		}
+		else
+		{
+			ft_putchar_fd(format[i], 1);
+			i++;
+		}
+	}
+	va_end(args);
 }
---
+++ ft_printf/ft_printf_utils.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albmart2 <albmart2@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 17:19:10 by albmart2          #+#    #+#             */
+/*   Updated: 2025/02/15 18:06:52 by albmart2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <unistd.h>
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (s != NULL)
+	{
+		while (s[i] != '\0')
+		{
+			write(fd, &s[i], 1);
+			i++;
+		}
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		write(fd, "-", 1);
+		write(fd, "2", 1);
+		ft_putnbr_fd(147483648, fd);
+	}
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		n += 48;
+		write(fd, &n, 1);
+	}
+}
